54138a6e793a9b53b2c40f3716e288204b5d2ec7
Nikolay Zemtsow <nickwebtm@gmail.com>
2024-07-17 05:36:02 +0500

    algo logic moved to utils

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const images = document.querySelectorAll('img')
-const lazy = document.querySelectorAll('#lazy')
-import { showModalWindow } from './utils/index.js';
+
+import { detectLazyTags } from './utils/index.js';
 
 /**
  * @typedef {boolean} 
@@ -14,36 +13,6 @@ import { showModalWindow } from './utils/index.js';
 
 let DEBUG_MODE = true;
 
-const lazyAlghorith = () => {
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.add("blur__image")
-    const loadData = setInterval(() => {
-      if (images[i].loading) {
-        console.log("loading",);
-      }
-      if (images[i].complete) {
-        images[i].classList.remove("blur__image")
-        console.log("loaded",);
-        clearInterval(loadData)
-      }
-    }, 100)
-  }
-};
-
-
-const detectLazyTags = () => {
-  if (lazy.length !== 0) {
-    showModalWindow({
-      message: `Yay we detected ${images.length} lazy :)`,
-      debug: DEBUG_MODE
-    })
-    return lazyAlghorith()
-  } else {
-    return showModalWindow({
-      message: `Lazy Images: Please enter some 1 lazy`,
-      debug: DEBUG_MODE
-    })
-  }
-}
-
-detectLazyTags()
+detectLazyTags({
+  debug: DEBUG_MODE
+})
